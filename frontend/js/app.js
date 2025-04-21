f2f04cc4e83fa3e3696f556d5d7b8b122473b98d
Константин <kostik.tikhonov.08@mail.ru>
2025-04-21 16:46:10 +0700

    Fix image paths and extensions

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -30,7 +30,7 @@ let products = [
         name: 'Набор "Вкусные моменты"',
         description: 'Весенний набор с мёдом и сладостями',
         price: 1200,
-        image: 'https://example.com/images/product1.jpg',
+        image: 'images/products/honey_set1.jpg',
         category: 'all'
     },
     {
@@ -38,7 +38,7 @@ let products = [
         name: 'Набор "Сладкое желание"',
         description: 'Подарочный набор с мёдом и чаем',
         price: 1890,
-        image: 'https://example.com/images/product2.jpg',
+        image: 'images/products/honey_set2.jpg',
         category: 'women'
     },
     {
@@ -46,7 +46,7 @@ let products = [
         name: 'Набор "Большой сюрприз"',
         description: 'Премиум набор с мёдом и деликатесами',
         price: 3670,
-        image: 'https://example.com/images/product3.jpg',
+        image: 'images/products/honey_set3.jpg',
         category: 'all'
     },
     {
@@ -54,7 +54,7 @@ let products = [
         name: 'Набор "Карусель-1" с фруктами',
         description: 'Подарочный набор с мёдом и сухофруктами',
         price: 2540,
-        image: 'https://example.com/images/product4.jpg',
+        image: 'images/products/honey_set4.jpg',
         category: 'all'
     }
 ];
